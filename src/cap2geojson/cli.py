29619f660232abd5833f3e26124999838e322a9e
RoryPTB <47696929+RoryPTB@users.noreply.github.com>
2024-09-04 14:38:45 +0200

    docs: add README + build: add CLI to pyproject + feat: write output to file in CLI

--- src/cap2geojson/cli.py
+++ src/cap2geojson/cli.py
@@ -1,5 +1,4 @@
 import click
-import json
 
 from cap2geojson import __version__, transform as transform_to_geojson
 
@@ -7,7 +6,7 @@ from cap2geojson import __version__, transform as transform_to_geojson
 @click.group()
 @click.version_option(version=__version__)
 def cli():
-    """cap2geojson"""
+    """cap2geojson: the tool to convert CAP alerts to GeoJSON"""
 
     pass
 
@@ -16,12 +15,16 @@ def cli():
 @click.pass_context
 @click.argument("cap_xml", type=click.File(mode="r", errors="ignore"))
 def transform(ctx, cap_xml) -> None:
-    """Convert a CAP alert to GeoJSON"""
+    """Convert a CAP alert XML file to GeoJSON"""
     cap = cap_xml.read()
+    filename = cap_xml.name.split(".")[0] + ".geojson"
 
     try:
         result = transform_to_geojson(cap)
-        click.echo(json.dumps(result, indent=2))
+        # Write the contents to a file
+        with open(filename, "w") as f:
+            f.write(result)
+        click.echo(f"GeoJSON file written to {filename}")
     except Exception as e:
         click.echo(f"Error: {e}")
         ctx.exit(1)
